1d58e5552f00f3be3158789b289d0972286b7ea1
Simon Fish <si@mon.fish>
2020-11-27 13:31:47 +0000

    Require all files, clean up gemspec

--- lib/remote_record.rb
+++ lib/remote_record.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 require 'active_support/concern'
+require 'remote_record/base'
+require 'remote_record/class_lookup'
+require 'remote_record/config'
+require 'remote_record/dsl'
+require 'remote_record/reference'
+require 'remote_record/version'
 
 # Generic interface for resources stored on external services.
 module RemoteRecord
