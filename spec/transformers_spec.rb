a7d949fdd0058688765d680353edea82776528d7
Simon Fish <si@mon.fish>
2021-01-14 11:17:51 +0000

    Raise error if direction is invalid

--- spec/transformers_spec.rb
+++ spec/transformers_spec.rb
@@ -7,6 +7,15 @@ RSpec.describe RemoteRecord::Transformers do
     let(:transformer) { described_class.new(data, direction) }
     subject(:expected_attribute) { transformer.transform[expected_attribute_name] }
 
+    context 'when the direction is not valid' do
+      let(:direction) { :left }
+      let(:data) { { userId: 1 } }
+
+      it 'is expected to raise an error' do
+        expect { transformer }.to raise_error ArgumentError
+      end
+    end
+
     context 'when the direction is up' do
       let(:direction) { :up }
       let(:expected_attribute_name) { :user_id }
